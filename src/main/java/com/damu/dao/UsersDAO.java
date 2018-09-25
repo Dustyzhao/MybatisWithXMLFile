528eb41d6bdeaa6411e6442b8e222a61751183ca
Dustyzhao <820328544@qq.com>
2018-09-25 15:57:35 +0800

    total commit once

---
+++ src/main/java/com/damu/dao/UsersDAO.java
@@ -0,0 +1,110 @@
+package com.damu.dao;
+
+import com.damu.entity.Users;
+import com.damu.utils.SqlSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.List;
+
+/**
+ *
+ * @author DUSTY
+ */
+public class UsersDAO {
+    private SqlSession sqlSession;
+    private List<Users> list;
+    private Users user;
+
+    private SqlSession getSession() {
+        sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+        return sqlSession;
+    }
+
+    /**
+     * 查询全部用户
+     * @return
+     */
+    public List<Users> findAll() {
+        try {
+            list = getSession().selectList("findUsers");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+        return list;
+    }
+
+    /**
+     * 查询单个用户根据编号
+     * @return
+     */
+    public Users findById(Integer id) {
+        try {
+            //这里的id因为是存在于动态sql，所以不能直接传入而是要封装进users对象再从中获取出来的
+            user = getSession().selectOne("findUsers", new Users(id));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+        return user;
+    }
+
+    /**
+     * 增加一个新用户数据到数据库的方法
+     * @return
+     */
+    public Users addUser(Users user) {
+        try {
+
+            // 返回值：是insert执行过程中影响的行数
+            getSession().insert("addUser", user);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+        return user;
+    }
+
+
+    /**
+     * 用于修改用户资料的方法
+     * @return
+     */
+    public Users updateUsers(Users user) {
+        try {
+            // 返回值：是insert执行过程中影响的行数
+            getSession().update("updateUser", user);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+        return user;
+    }
+
+    /**
+     * 用于修改用户资料的方法
+     * @return
+     */
+    public void delUsers(Integer id) {
+        try {
+
+            // 返回值：是insert执行过程中影响的行数
+            getSession().delete("delUser", id);
+
+            sqlSession.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+}
